e0d12fd6ccfbd7e90caa5840dbb1b1700be682f8
Yustas <goosseff@yandex.ru>
2020-02-20 17:51:28 +0300

    amended Rainbow

--- PrintCross/src/PrintCross.java
+++ PrintCross/src/PrintCross.java
@@ -5,7 +5,7 @@ public class PrintCross {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     System.out.println("Введите количество строк, из которых будет состоять крестик");
-    //hello git
+    //hello git 5
     int n = s.nextInt();
     s.close();
 
--- Rainbow/src/Rainbow.java
+++ Rainbow/src/Rainbow.java
@@ -4,31 +4,41 @@ public class Rainbow {
 
   public static void main(String[] args) {
 
-  String [] colors = {"Красный", "Оранжевый", "Желтый", "Зеленый", "Голубой", "Синий", "Фиолетовый"};
-  String [] reverseColors = new String [7];
-    int j = 0;
-    String massege = "Распечатаем элементы первоначального массива <colors>";
-    String massege1 = "Распечатаем элементы массива <reverseColors>";
+    String[] colors = {"Красный", "Оранжевый", "Желтый", "Зеленый", "Голубой", "Синий",
+        "Фиолетовый"};
 
-    for(int i = colors.length - 1; i >= 0;  i--){
-      reverseColors [j] = colors [i];
-      j += 1;
-    }
+    int m = colors.length - 1;
+    String s = null;
+    String massege = "Распечатаем элементы массива <colors> до процедуры";
+    String massege1 = "Распечатаем элементы массива <colors> после процедуры";
 
     System.out.println(massege);
-    for (int a = 0; a < massege.length(); a++)
+    for (int a = 0; a < massege.length(); a++) {
       System.out.print("=");
-      System.out.println();
-    for(int b = 0; b < colors.length; b++) {
+    }
+    System.out.println();
+    for (int b = 0; b < colors.length; b++) {
       System.out.println(colors[b]);
     }
-      System.out.println();
-      System.out.println(massege1);
-      for (int a = 0; a < massege1.length(); a++)
-        System.out.print("=");
-      System.out.println();
-    for(int y = 0; y < reverseColors.length; y++) {
-      System.out.println(reverseColors[y]);
+
+    for (int i = 0; i < colors.length; i++) {
+      if (i == m) {
+        break;
+      }
+      s = colors[i];
+      colors[i] = colors[m];
+      colors[m] = s;
+      m--;
+    }
+
+    System.out.println();
+    System.out.println(massege1);
+    for (int a = 0; a < massege1.length(); a++) {
+      System.out.print("=");
+    }
+    System.out.println();
+    for (int b = 0; b < colors.length; b++) {
+      System.out.println(colors[b]);
     }
   }
 }
