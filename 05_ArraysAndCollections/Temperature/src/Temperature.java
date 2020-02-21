2d3aebb3a79d20301752523abc4ade1edb9be9b8
Yustas <goosseff@yandex.ru>
2020-02-21 18:16:41 +0300

    corrected Temperature

--- 05_ArraysAndCollections/Temperature/src/Temperature.java
+++ 05_ArraysAndCollections/Temperature/src/Temperature.java
@@ -3,22 +3,31 @@
 public class Temperature {
 
   public static void main(String[] args) {
-    int z = 0;
-    double result = 0;
-    double[] temperature = new double[30];
+    final int PATIENTS = 30;        // количество пациентов
+    final double MAX_TEMP = 40.0;   // максимальная температура генерации пациента
+    final double FACTOR_MAX = 8.0;   // коэффициент для генерации максимальной температуры
+    final double MIN_TEMP = 32.0;   // минимальная температура генерации пациента
+    final double MIN_HEALTH = 36.2; // минимальная температура здорового пациента
+    final double MAX_HEALTH = 36.9; // максимальная температура здорового пациента
+
+    int z = 0; // количество пациентов, температура которых находится в промежутке между 36,2 и 36,9
+    double result = 0; // переменная средней температуры всех пациентов
+    double sum = 0; // переменная суммарной температуры всех пациентов
+
+    double[] temperature = new double[PATIENTS];
 
     for (int i = 0; i < temperature.length; i++) {
-      temperature[i] = 32.0 + 8.0 * Math.random();
+      temperature[i] = MIN_TEMP + FACTOR_MAX * Math.random();
       // System.out.println(temperature[i]);
 
-      result = result + temperature[i];
+      sum = sum + temperature[i];
 
-      if (temperature[i] >= 36.2 && temperature[i] <= 36.9) {
+      if (temperature[i] >= MIN_HEALTH && temperature[i] <= MAX_HEALTH) {
         z++;
       }
     }
 
-    result = result / temperature.length;
+    result = sum / temperature.length;
     System.out.printf("Средняя температура по больнице: " + "%.1f", result);
     System.out.println();
     System.out.println("Количество здоровых пациентов: " + z);
