44ce9b032c6f022c37db398a71fecc0afc3d294d
Yustas <goosseff@yandex.ru>
2020-02-24 13:30:36 +0300

    the array starts with zero

--- 05_ArraysAndCollections/PrintCross/src/PrintCross.java
+++ 05_ArraysAndCollections/PrintCross/src/PrintCross.java
@@ -5,17 +5,17 @@ public class PrintCross {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     System.out.println("Введите количество строк, из которых будет состоять крестик");
-    int n = s.nextInt();
+    int n = s.nextInt() - 1;
     s.close();
 
     String[][] cross = new String[n + 1][n + 1];
 
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
+    for (int i = 0; i <= n; i++) {
+      for (int j = 0; j <= n; j++) {
         if (j == i) {
           System.out.print("X");
           cross[i][j] = "O";
-        } else if (j == n - (i - 1)) {
+        } else if (j == n - (i)) {
           System.out.print("X");
           cross[i][j] = "O";
         } else {
@@ -27,8 +27,8 @@ public class PrintCross {
     }
 
     System.out.println();
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
+    for (int i = 0; i <= n; i++) {
+      for (int j = 0; j <= n; j++) {
         System.out.print(cross[i][j]);
       }
       System.out.println();
