55eb84f6abc74e7600ec44d0bf92bfd7cb6b27b4
Yustas <goosseff@yandex.ru>
2020-03-19 18:41:26 +0300

    interim code 5.3 new

---
+++ 05_ArraysAndCollections/EmailList/src/Bootloader.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class Bootloader {
+
+  public static void main(String[] args) throws IOException {
+
+    //final String COMMAND_TEXT = "(ADD\\s+)(.+[@]{1}(^[^@]{1}.+$)[.]{1}.+)";
+    //final String COMMAND_TEXT = "((@{1})([^@]{1}.+$))";
+   // final String COMMAND_TEXT = "(ADD\\s+)(^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$)";
+   // final String COMMAND_TEXT = "(ADD)\\s+(^(.+)@(.+)$)";
+    final String COMMAND_LIST = "LIST";
+    final String COMMAND_END = "END";
+    String command = null;
+
+    printUsage();
+
+    EmailList myEmailList = new EmailList();
+
+    myEmailList.emailList.add("111@mail.ru");
+    myEmailList.emailList.add("222@mail.ru");
+    myEmailList.emailList.add("333@mail.ru");
+    myEmailList.emailList.add("444@mail.ru");
+    myEmailList.emailList.add("555@mail.ru");
+    myEmailList.emailList.add("aaa@mail.ru");
+    myEmailList.emailList.add("bbb@mail.ru");
+    myEmailList.emailList.add("ccc@mail.ru");
+    myEmailList.emailList.add("ddd@mail.ru");
+    myEmailList.emailList.add("eee@mail.ru");
+
+    while (true) {
+      Scanner scanner = new Scanner(System.in);
+      command = scanner.nextLine();
+
+      if (command.matches(COMMAND_LIST)) {
+        myEmailList.printList();
+      } else if (command.matches(COMMAND_TEXT)) {
+        String emailText = command.replaceAll(COMMAND_TEXT, "$2");
+        myEmailList.addEmail(emailText);
+      } else if (command.matches(COMMAND_END)) {
+        System.out.println("Работа программы окончена");
+        return;
+      } else {
+        System.out.println("Команда не распознана. Введите еще раз.");
+      }
+    }
+  }
+
+  public static void printUsage() {
+    System.out.println("Программа содержит перечень e-mail адресов. Если Вы хотите добавить е-mail адрес, его можно добавить через консоль командой ADD");
+    System.out.println("Формат ввода команды:  ADD e-mail");
+    System.out.println("Чтобы напечатать весь список - используйте команду LIST");
+    System.out.println("Если Вы хотите прекратить выполнение программы, наберите команду: END ");
+    System.out.println("Вызовите команду : ");
+  }
+}
