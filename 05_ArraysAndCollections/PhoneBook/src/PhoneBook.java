bcc39902553b8aa58e3fa482af1ac73731a0d22e
Yustas <goosseff@yandex.ru>
2020-03-31 15:21:50 +0300

    HomeWork 5.4 working code

--- 05_ArraysAndCollections/PhoneBook/src/PhoneBook.java
+++ 05_ArraysAndCollections/PhoneBook/src/PhoneBook.java
@@ -5,13 +5,20 @@ import java.util.TreeMap;
 
 public class PhoneBook {
 
+  TreeMap<String, String> phoneBook = new TreeMap<>();
+
   public static void main(String[] args) throws IOException {
+
+    PhoneBook phoneBook = new PhoneBook();
+    phoneBook.run();
+  }
+
+  void run() {
 
     final String COMMAND_LIST = "(?i)LIST";
     final String COMMAND_NAME = "[([a-zA-Zа-яА-Я]+)(\\s+)]+";
     final String COMMAND_NUMBER = "[0-9]+";
 
-    TreeMap<String, String> phoneBook = new TreeMap<>();
     phoneBook.put("Иванов Иван Иванович", "111111");
     phoneBook.put("Петров Петр Петрович", "222222");
     phoneBook.put("Сидоров Сидор Сидорович", "333333");
@@ -20,8 +27,8 @@ public class PhoneBook {
 
     System.out.println("Вы пользуетесь программой <Телефонная книга>");
     System.out.println("Если Вы хотите увидеть все данные <Телефонной книги> ввелите команду LIST");
-    System.out.println(
-        "Если Вы хотите добавить в <Телефонную книгу> контакт - введите ФИО и номер телефона абонента через пробел");
+    System.out.println("Если Вы хотите добавить в <Телефонную книгу> контакт:" + "\n"
+            + "введите ФИО и если абонента с такими данными не существует - номер телефона");
     System.out.println("Введите команду:");
 
     Scanner scanner = new Scanner(System.in);
@@ -34,29 +41,31 @@ public class PhoneBook {
       if (input.matches(COMMAND_LIST)) {
         printMap(phoneBook);
         continue;
+
       } else if (input.matches(COMMAND_NAME)) {
         if (phoneBook.containsKey(input)) {
           printAbonentByName(input);
         } else {
           System.out.println("Контакта с такими данными не существует");
           System.out.println("Введите номер телефона указанного абонента");
-          String phoneNumber = scanner.nextLine();
-          phoneBook.put(input, phoneNumber);
-          System.out.println("Данные абонента добавлены в телефонный справочник");
-          printMap(
-              phoneBook);           // техническая распечатка, чтобы убедиться, что данные корректно добавлены
+          String command_input2 = scanner.nextLine();
+          if (command_input2.matches(COMMAND_NUMBER)) {
+            phoneBook.put(input, getUserInput(command_input2));
+            printMap(phoneBook);
+          } else {
+            System.out.println("Вы не указали правильный номер телефона. Введите команду еще раз.");
+          }
         }
+
       } else if (input.matches(COMMAND_NUMBER)) {
         if (phoneBook.containsValue(input)) {
           printAbonentByNumber(input);
         } else {
-          System.out.println("Контакта с такими данными не существует");
+          System.out.println("Контакта с таким номером не существует");
           System.out.println("Введите ФИО указанного абонента");
-          String phoneName = scanner.nextLine();
-          phoneBook.put(phoneName, input);
-          System.out.println("Данные абонента добавлены в телефонный справочник");
-          printMap(
-              phoneBook);           // техническая распечатка, чтобы убедиться, что данные корректно добавлены
+          String command_input2 = scanner.nextLine();
+          phoneBook.put(getUserInput(command_input2), input);
+          printMap(phoneBook);
         }
       } else {
         System.out.println("Команда не распознана. Введите еще раз.");
@@ -64,12 +73,16 @@ public class PhoneBook {
     }
   }
 
+  private String getUserInput(String command_input2) {
+    System.out.println("Данные абонента добавлены в телефонный справочник");
+    return command_input2;
+  }
 
-  private static void printAbonentByName(String input) {
+  private void printAbonentByName(String input) {
     System.out.println("Телефон абонента:  " + phoneBook.get(input));
   }
 
-  private static void printAbonentByNumber(String input) {
+  private void printAbonentByNumber(String input) {
     for (Entry<String, String> entry : phoneBook.entrySet()) {
       String key = entry.getKey();
       String value = entry.getValue();
