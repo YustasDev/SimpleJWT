3c1701a78496dec4c1aff3694eefacb42b22baf9
Yustas <goosseff@yandex.ru>
2020-04-04 12:50:21 +0300

    HomeWork 5.5 revised code

--- 05_ArraysAndCollections/GeneratorN/src/GeneratorN.java
+++ 05_ArraysAndCollections/GeneratorN/src/GeneratorN.java
@@ -1,4 +1,5 @@
 import static java.lang.System.nanoTime;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -26,59 +27,54 @@ public class GeneratorN {
           carNumbers.add(number);
           carNumHashSet.add(number);
           carNumTreeSet.add(number);
+        }
       }
     }
-  }
     long end = System.currentTimeMillis();
     System.out.println("Длительность процесса формирования массива " + (end - start) + "  ms");
-    System.out.println("Размер массива  " + carNumbers.size() + "  элементов" );
+    System.out.println("Размер массива  " + carNumbers.size() + "  элементов");
 
     long start1 = System.nanoTime();
-    if (carNumbers.contains("А999АА161")) {
-      long end1 = System.nanoTime();
-      System.out.println("Поиск перебором: номер найден, поиск занял " + (end1 - start1) + "  ns");
-    }
-    else {
-      long end11 = System.nanoTime();
-      System.out.println("Поиск перебором: номер не найден, поиск занял  " + (end11 - start1) + "  ns");
+    boolean isFound = carNumbers.contains("А999АА161");
+    long end1 = System.nanoTime();
+    long time1 = end1 - start1;
+    if (isFound) {
+      System.out.println("Поиск перебором: номер найден, поиск занял " + time1 + "  ns");
+    } else {
+      System.out.println("Поиск перебором: номер не найден, поиск занял  " + time1 + "  ns");
     }
 
     Collections.sort(carNumbers);
     long start2 = System.nanoTime();
-    int indexTrue = Collections.binarySearch(carNumbers, "А999АА161");
-    if (indexTrue != -1) {
-      long end2 = System.nanoTime();
-      System.out.println("Бинарный поиск: номер найден, поиск занял  " + (end2 - start2) + "  ns");
-    }
-    else {
-      long end22 = System.nanoTime();
-      System.out.println("Бинарный поиск: номер не найден, поиск занял  " + (end22 - start2) + "  ns");
+    int indexTrue = Collections.binarySearch(carNumbers, "Э999АА161");
+    long end2 = System.nanoTime();
+    long time2 = end2 - start2;
+    if (indexTrue < 0) {
+      System.out.println("Бинарный поиск: номер не найден, поиск занял  " + time2 + "  ns");
+    } else {
+      System.out.println("Бинарный поиск: номер найден, поиск занял  " + time2 + "  ns");
     }
 
     long start3 = System.nanoTime();
-    if (carNumHashSet.contains("А999АА161")) {
-      long end3 = System.nanoTime();
-      System.out.println("Поиск в HashSet: номер найден, поиск занял " + (end3 - start3) + "  ns");
-    }
-    else {
-      long end31 = System.nanoTime();
-      System.out.println("Поиск в HashSet: номер не найден, поиск занял  " + (end31 - start3) + "  ns");
+    boolean isFound3 = carNumHashSet.contains("А999АА161");
+    long end3 = System.nanoTime();
+    long time3 = end3 - start3;
+    if (isFound3) {
+      System.out.println("Поиск в HashSet: номер найден, поиск занял " + time3 + "  ns");
+    } else {
+      System.out.println("Поиск в HashSet: номер не найден, поиск занял  " + time3 + "  ns");
     }
 
-    long start4ns = System.nanoTime();
-    long start4 = System.currentTimeMillis();
-    if (carNumTreeSet.contains("А999АА161")) {
-      long end4ns = System.nanoTime();
-      long end4 = System.currentTimeMillis();
-      System.out.println("Поиск в TreeSet: номер найден, поиск занял " + (end4ns - start4ns) + "  ns");
-      System.out.println("Поиск в TreeSet: номер найден, поиск занял " + (end4 - start4) + "  ms");
+    long start4 = System.nanoTime();
+    boolean isFound4 = carNumTreeSet.contains("А999АА161");
+    long end4 = System.nanoTime();
+    Long time4 = end4 - start4;
+    if (isFound4) {
+      System.out.println("Поиск в TreeSet: номер найден, поиск занял " + time4 + "  ns");
+    } else {
+      System.out.println("Поиск в TreeSet: номер найден, поиск занял " + time4 + "  ms");
     }
-    else {
-      long end41ns = System.nanoTime();
-      long end41 = System.currentTimeMillis();
-      System.out.println("Поиск в TreeSet: номер не найден, поиск занял " + (end41ns - start4) + "  ns");
-      System.out.println("Поиск в TreeSet: номер не найден, поиск занял  " + (end41 - start4) + "  ms");
-    }
+
   }
 }
 
