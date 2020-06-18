67aa2731ee0fc4ad5b0b881eac6503b8bd6910cb
Yustas <goosseff@yandex.ru>
2020-06-18 21:01:27 +0300

    Home_work 8.4 with Exceptions in Main

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -2,54 +2,37 @@ import java.util.HashMap;
 
 public class CustomerStorage {
 
-    final String TELEFONNUMBER = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3,4}\\)?[\\- ]?)?[\\d\\- ]{5,10}$";
-    final String REGEXEMAIL = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
-    private HashMap<String, Customer> storage;
+  final String TELEFONNUMBER = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3,4}\\)?[\\- ]?)?[\\d\\- ]{5,10}$";
+  final String REGEXEMAIL = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+  private HashMap<String, Customer> storage;
 
-    public CustomerStorage()
-    {
-        storage = new HashMap<>();
-    }
+  public CustomerStorage() {
+    storage = new HashMap<>();
+  }
 
-    public void addCustomer(String data) {
-        String[] components = data.split("\\s+");
-        if (components.length > 4 || components.length < 4) {
-            try {
-                throw new Exception("The number of elements in a command is not equal to four!");
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println(Main.helpText);
-                return;
-            }
-        }
-        if (components.length == 4) {
-            String name = components[0] + " " + components[1];
-            if (components[3].matches(TELEFONNUMBER) && components[2].matches(REGEXEMAIL)) {
-                storage.put(name, new Customer(name, components[3], components[2]));
-            } else {
-                try {
-                    throw new Exception("Incorrect phone number or email address");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    System.out.println(Main.helpText);
-                    return;
-                }
-            }
-        }
+  public void addCustomer(String data) throws Exception {
+    String[] components = data.split("\\s+");
+    if (components.length > 4 || components.length < 4) {
+      throw new Exception("The number of elements in a command is not equal to four!");
+    } else if (components.length == 4) {
+      String name = components[0] + " " + components[1];
+      if (components[3].matches(TELEFONNUMBER) && components[2].matches(REGEXEMAIL)) {
+        storage.put(name, new Customer(name, components[3], components[2]));
+      } else {
+        throw new Exception("Incorrect phone number or email address");
+      }
     }
+  }
 
-    public void listCustomers()
-    {
-        storage.values().forEach(System.out::println);
-    }
+  public void listCustomers() {
+    storage.values().forEach(System.out::println);
+  }
 
-    public void removeCustomer(String name)
-    {
-        storage.remove(name);
-    }
+  public void removeCustomer(String name) throws Exception {
+    storage.remove(name);
+  }
 
-    public int getCount()
-    {
-        return storage.size();
-    }
+  public int getCount() {
+    return storage.size();
+  }
 }
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -1,42 +1,45 @@
 import java.util.Scanner;
 
-public class Main
-{
-    private static String addCommand = "add Василий Петров " +
-            "vasily.petrov@gmail.com +79215637722";
-    private static String commandExamples = "\t" + addCommand + "\n" +
-            "\tlist\n\tcount\n\tremove Василий Петров";
-    private static String commandError = "Wrong command! Available command examples: \n" +
-            commandExamples;
-    protected static String helpText = "Command examples:\n" + commandExamples;
+public class Main {
 
-    public static void main(String[] args)
-    {
-        Scanner scanner = new Scanner(System.in);
-        CustomerStorage executor = new CustomerStorage();
-        for(;;)
-        {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("list")) {
-                executor.listCustomers();
-            }
-            else if(tokens[0].equals("remove"))
-            {
-                executor.removeCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            }
-            else if(tokens[0].equals("help")) {
-                System.out.println(helpText);
-            }
-            else {
-                System.out.println(commandError);
-            }
+  private static String addCommand = "add Василий Петров " +
+      "vasily.petrov@gmail.com +79215637722";
+  private static String commandExamples = "\t" + addCommand + "\n" +
+      "\tlist\n\tcount\n\tremove Василий Петров";
+  private static String commandError = "Wrong command! Available command examples: \n" +
+      commandExamples;
+  private static String helpText = "Command examples:\n" + commandExamples;
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    CustomerStorage executor = new CustomerStorage();
+    for (; ; ) {
+      String command = scanner.nextLine();
+      String[] tokens = command.split("\\s+", 2);
+      if (tokens[0].equals("add")) {
+        try {
+          executor.addCustomer(tokens[1]);
+        } catch (Exception e) {
+          e.printStackTrace();
+          System.out.println(helpText);
         }
+      } else if (tokens[0].equals("list")) {
+        executor.listCustomers();
+      } else if (tokens[0].equals("remove")) {
+        try {
+          executor.removeCustomer(tokens[1]);
+        } catch (Exception e) {
+          e.printStackTrace();
+          System.err.println("Error input parameter of the method <removeCustomer>");
+          System.out.println(helpText);
+        }
+      } else if (tokens[0].equals("count")) {
+        System.out.println("There are " + executor.getCount() + " customers");
+      } else if (tokens[0].equals("help")) {
+        System.out.println(helpText);
+      } else {
+        System.out.println(commandError);
+      }
     }
+  }
 }
