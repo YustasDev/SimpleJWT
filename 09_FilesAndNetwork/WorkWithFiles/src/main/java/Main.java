409de08f32607c91c722b69ce20e71ab6058e9e2
Yustas <goosseff@yandex.ru>
2020-07-22 20:16:40 +0300

    main is billet

--- 09_FilesAndNetwork/WorkWithFiles/src/main/java/Main.java
+++ 09_FilesAndNetwork/WorkWithFiles/src/main/java/Main.java
@@ -1,5 +1,37 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 
 public class Main {
 
+  private static Scanner scanner;
+
+  public static void main(String[] args) {
+
+    scanner = new Scanner(System.in);
+
+    String path = receivePath("Введите путь к папке. Образец: D:/Games");
+    File folder = new File(path);
+    File[] files = folder.listFiles();
+
+    // Stream<File> streamArr = Stream.of(files);
+    //streamArr.forEach(x -> System.out.println(x));
+
+    Stream.of(files).forEach(x -> System.out.println(x));
+
+
+  }
+
+  private static String receivePath(String message) {
+
+    for (; ; ) {
+      System.out.println(message);
+      String line = scanner.nextLine().trim();
+      if (line != null) {
+        return line;
+      }
+      System.out.println("Путь к папке (директории) не указан");
+    }
+  }
 }
