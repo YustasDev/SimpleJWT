2d7cbc420e5dd8fee868b7b5d4029e41b0f8d8a7
Yustas <goosseff@yandex.ru>
2020-07-24 12:34:40 +0300

    working code 1.0

--- 09_FilesAndNetwork/WorkWithFiles/src/main/java/Main.java
+++ 09_FilesAndNetwork/WorkWithFiles/src/main/java/Main.java
@@ -1,43 +1,57 @@
+import static javax.print.attribute.standard.MediaSizeName.C;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
 
 public class Main {
 
+  private static ArrayList<File> listWithFileNames = new ArrayList<>();
   private static Scanner scanner;
 
   public static void main(String[] args) {
 
-   for (; ;) {
-     scanner = new Scanner(System.in);
-     mark1:
-     {
-       String path = receivePath("Введите путь к папке. Образец: D:/Games");
-       File folder = new File(path);
+    for (; ; ) {
+      scanner = new Scanner(System.in);
 
-       File[] files;
-       if (folder.isDirectory()) {
-         files = folder.listFiles();
-       } else {
-         System.out.println("Указан неправильный путь к папке");
-         break mark1;
-       }
+        String path = receivePath("Введите путь к папке. Образец: D:/Games");
 
+     try {
+      getListFiles(path);
 
-       for (File f : files)
-         System.out.println("Элемент" + files);
+        for (File file : listWithFileNames) {
+          System.out.println(file.getName());
+        }
 
+       Double partitionSize = listWithFileNames.stream().mapToDouble(x -> x.length()).sum();
+       System.out.println("Размер указанной директории =  " + partitionSize/1024/1024 + " Мб");
 
-
-       //List<Long> collect =
-           Stream.of(files).map(x -> x.getTotalSpace()).forEach(x -> System.out.println(x));
-       Stream.of(files).forEach(x -> System.out.println(x));
-         //  collect(Collectors.toList());
-      // System.out.println(collect);
-
+      }
+     catch (Exception ex) {
+       ex.printStackTrace();
+       System.out.println("Путь к папке (директории) не указан или указан неправильно");
      }
-   }
+      }
+    }
+
+
+  public static void getListFiles(String str) {
+    File folder = new File(str);
+    for (File s : folder.listFiles()) {
+      if (s.isFile()) {
+        listWithFileNames.add(s);
+      } else if (s.isDirectory()) {
+        getListFiles(s.getAbsolutePath());
+      }
+      else {System.out.println("Указан неправильный путь к папке");}
+    }
   }
 
   private static String receivePath(String message) {
@@ -48,7 +62,6 @@ public class Main {
       if (line != null) {
         return line;
       }
-      System.out.println("Путь к папке (директории) не указан");
-    }
+     }
   }
 }
