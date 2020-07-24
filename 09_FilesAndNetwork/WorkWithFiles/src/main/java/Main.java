4c2c2db07847f2fd10e889fd9a44214902a8f894
Yustas <goosseff@yandex.ru>
2020-07-24 15:25:35 +0300

    working code 2.0

--- 09_FilesAndNetwork/WorkWithFiles/src/main/java/Main.java
+++ 09_FilesAndNetwork/WorkWithFiles/src/main/java/Main.java
@@ -1,14 +1,7 @@
-import static javax.print.attribute.standard.MediaSizeName.C;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 
 public class Main {
@@ -21,36 +14,44 @@ public class Main {
     for (; ; ) {
       scanner = new Scanner(System.in);
 
-        String path = receivePath("Введите путь к папке. Образец: D:/Games");
+      String path = receivePath("Введите путь к папке. Образец: D:/Games");
 
-     try {
-      getListFiles(path);
+      try {
+        getListFiles(path);
 
-        for (File file : listWithFileNames) {
-          System.out.println(file.getName());
+        Double partitionSize = listWithFileNames.stream().mapToDouble(x -> x.length()).sum();
+        if (partitionSize < 1024) {
+          System.out
+              .println("Размер указанной директории  " + path + " = " + partitionSize + " Байт");
+        } else if (partitionSize < 1048576) {
+          System.out.println(
+              "Размер указанной директории  " + path + " = " + partitionSize / 1024 + " Kб");
+        } else if (partitionSize < 1073741824) {
+          System.out.println(
+              "Размер указанной директории  " + path + " = " + partitionSize / 1024 / 1024 + " Мб");
+        } else {
+          System.out.println(
+              "Размер указанной директории  " + path + " = " + partitionSize / 1024 / 1024 / 1024
+                  + " Гб");
         }
-
-       Double partitionSize = listWithFileNames.stream().mapToDouble(x -> x.length()).sum();
-       System.out.println("Размер указанной директории =  " + partitionSize/1024/1024 + " Мб");
-
+      } catch (Exception ex) {
+        ex.printStackTrace();
+        System.out.println("Путь к папке (директории) не указан или указан неправильно");
       }
-     catch (Exception ex) {
-       ex.printStackTrace();
-       System.out.println("Путь к папке (директории) не указан или указан неправильно");
-     }
-      }
+      listWithFileNames.clear();
     }
+  }
 
 
   public static void getListFiles(String str) {
     File folder = new File(str);
-    for (File s : folder.listFiles()) {
-      if (s.isFile()) {
-        listWithFileNames.add(s);
-      } else if (s.isDirectory()) {
-        getListFiles(s.getAbsolutePath());
+    File[] files = folder.listFiles();
+    for (File file : files) {
+      if (file.isFile()) {
+        listWithFileNames.add(file);
+      } else if (file.isDirectory()) {
+        getListFiles(file.getAbsolutePath());
       }
-      else {System.out.println("Указан неправильный путь к папке");}
     }
   }
 
@@ -62,6 +63,6 @@ public class Main {
       if (line != null) {
         return line;
       }
-     }
+    }
   }
 }
