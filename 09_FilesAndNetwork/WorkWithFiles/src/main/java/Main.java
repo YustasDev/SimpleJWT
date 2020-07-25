45a3c3bec4ec4ee60cb6f06516e7cf9ab430339e
Yustas <goosseff@yandex.ru>
2020-07-25 22:08:34 +0300

    working code with correction

--- 09_FilesAndNetwork/WorkWithFiles/src/main/java/Main.java
+++ 09_FilesAndNetwork/WorkWithFiles/src/main/java/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class Main {
 
-  private static ArrayList<File> listWithFileNames = new ArrayList<>();
   private static Scanner scanner;
+
 
   public static void main(String[] args) {
 
@@ -15,45 +15,50 @@ public class Main {
       scanner = new Scanner(System.in);
 
       String path = receivePath("Введите путь к папке. Образец: D:/Games");
+      File folder = new File(path);
 
       try {
-        getListFiles(path);
+        double partitionSize = getListFiles(folder);
 
-        Double partitionSize = listWithFileNames.stream().mapToDouble(x -> x.length()).sum();
-        if (partitionSize < 1024) {
-          System.out
-              .println("Размер указанной директории  " + path + " = " + partitionSize + " Байт");
-        } else if (partitionSize < 1048576) {
-          System.out.println(
-              "Размер указанной директории  " + path + " = " + partitionSize / 1024 + " Kб");
-        } else if (partitionSize < 1073741824) {
-          System.out.println(
-              "Размер указанной директории  " + path + " = " + partitionSize / 1024 / 1024 + " Мб");
-        } else {
-          System.out.println(
-              "Размер указанной директории  " + path + " = " + partitionSize / 1024 / 1024 / 1024
-                  + " Гб");
-        }
+        printSizeFile(partitionSize, path);
+
       } catch (Exception ex) {
         ex.printStackTrace();
         System.out.println("Путь к папке (директории) не указан или указан неправильно");
       }
-      listWithFileNames.clear();
     }
   }
 
+  private static void printSizeFile(double partitionSize, String path) {
 
-  public static void getListFiles(String str) {
-    File folder = new File(str);
-    File[] files = folder.listFiles();
+    if (partitionSize < 1024) {
+      System.out
+          .println("Размер указанной директории  " + path + " = " + partitionSize + " Байт");
+    } else if (partitionSize < 1048576) {
+      System.out.printf("Размер указанной директории \"%s\" равен %.2f Кб \n", path,
+          partitionSize / 1024);
+    } else if (partitionSize < 1073741824) {
+      System.out.printf("Размер указанной директории \"%s\" равен %.2f Мб \n", path,
+          partitionSize / 1024 / 1024);
+    } else {
+      System.out.printf("Размер указанной директории \"%s\" равен %.2f Гб \n", path,
+          partitionSize / 1024 / 1024 / 1024);
+    }
+  }
+
+  public static double getListFiles(File dir) {
+    double size = 0;
+    File[] files = dir.listFiles();
     for (File file : files) {
       if (file.isFile()) {
-        listWithFileNames.add(file);
-      } else if (file.isDirectory()) {
-        getListFiles(file.getAbsolutePath());
+        size += file.length();
+      } else {
+        size += getListFiles(file);
       }
     }
+    return size;
   }
+
 
   private static String receivePath(String message) {
 
