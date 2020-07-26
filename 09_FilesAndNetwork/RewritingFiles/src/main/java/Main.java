28e6699fb97b984c3751231bf0f90e41018ff97c
Yustas <goosseff@yandex.ru>
2020-07-27 00:18:54 +0300

    fully operational code

--- 09_FilesAndNetwork/RewritingFiles/src/main/java/Main.java
+++ 09_FilesAndNetwork/RewritingFiles/src/main/java/Main.java
@@ -1,18 +1,15 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Main {
 
   private static Scanner scanner;
-  ArrayList<String> selectFiles = new ArrayList<>();
 
   public static void main(String[] args) throws IOException {
 
@@ -26,17 +23,7 @@ public class Main {
 
       Path sourcePath = Paths.get(sourse);
       Path destinationPath = Paths.get(dest);
-      copyFolder (sourcePath, destinationPath);
-
-//      try {
-//        Files.copy(sourcePath, destinationPath,
-//            StandardCopyOption.REPLACE_EXISTING);
-//      } catch (FileAlreadyExistsException e) {
-//        //destination file already exists
-//      } catch (IOException e) {
-//        //something else went wrong
-//        e.printStackTrace();
-//      }
+      copyFolder(sourcePath, destinationPath, dest);
     }
   }
 
@@ -52,52 +39,21 @@ public class Main {
   }
 
 
-  private static void copyFolder(Path source, Path target) throws IOException {
+  private static void copyFolder(Path source, Path target, String dest) throws IOException {
     try (Stream<Path> stream = Files.walk(source)) {
       stream.forEach((child) -> {
         try {
           Path relative = source.relativize(child);
           Path destination = target.resolve(relative);
+          new File(String.valueOf(dest)).mkdirs();
           if (!destination.toFile().isDirectory()) {
             Files.copy(child, destination, StandardCopyOption.REPLACE_EXISTING);
-            }
+          }
         } catch (Exception ex) {
-          throw new IllegalStateException(ex);
+          ex.printStackTrace();
         }
       });
     }
   }
 }
 
-
-
-//
-//
-//
-//
-//    File folder = new File("путь");
-//    File[] listOfFiles = folder.listFiles();
-//
-//    for (File f : listOfFiles) {
-//      selectFiles.add(f + "");
-//    }
-//
-//    File source = new File(" " + selectFiles);// ПОЛУЧАЮ СПИСОК ФАЙЛОВ ПРИСВАИВАЮ ПЕРЕМЕННУЮ С ФАЙЛАМИ
-//    File dest = new File("путь");// ПРОПИСЫВАЮ ПУТЬ КУДА КОПИРОВАТЬ
-//    copy(source, dest);
-//  }
-//
-//  private static void copyDir(String sourceDirName, String targetSourceDir) {
-//    File folder = new File(sourceDirName);
-//
-//    File[] listOfFiles = folder.listFiles();
-//
-//    Path destDir = Paths.get(targetSourceDir);
-//    if (listOfFiles != null)
-//      for (File file : listOfFiles)
-//        Files.copy(file.toPath(), destDir.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
-//
-//  }
-//
-//
-//}
