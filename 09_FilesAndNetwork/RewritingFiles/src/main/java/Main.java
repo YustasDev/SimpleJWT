a647f8ce389d40c6643f54ae3237379eb2127cc2
Yustas <goosseff@yandex.ru>
2020-07-27 18:11:29 +0300

    fully operational code 1.0

--- 09_FilesAndNetwork/RewritingFiles/src/main/java/Main.java
+++ 09_FilesAndNetwork/RewritingFiles/src/main/java/Main.java
@@ -7,6 +7,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+
 public class Main {
 
   private static Scanner scanner;
@@ -17,18 +18,17 @@ public class Main {
       scanner = new Scanner(System.in);
 
       String sourse = receivePath(
-          "Введите путь к папке, которую надо переписать. Образец: С:/Games/Star Wars");
+          "Введите путь к папке, которую надо копировать. Образец: С:/Games/StarWars");
       String dest = receivePath(
           "Введите путь, куда надо переписать. Образец: D:/Archive/Games");
 
       Path sourcePath = Paths.get(sourse);
       Path destinationPath = Paths.get(dest);
-      copyFolder(sourcePath, destinationPath, dest);
+      copyFolder(sourcePath, destinationPath);
     }
   }
 
   private static String receivePath(String message) {
-
     for (; ; ) {
       System.out.println(message);
       String line = scanner.nextLine().trim();
@@ -38,22 +38,31 @@ public class Main {
     }
   }
 
-
-  private static void copyFolder(Path source, Path target, String dest) throws IOException {
+  private static void copyFolder(Path source, Path target) {
+    Boolean check = true;
     try (Stream<Path> stream = Files.walk(source)) {
       stream.forEach((child) -> {
         try {
           Path relative = source.relativize(child);
           Path destination = target.resolve(relative);
-          new File(String.valueOf(dest)).mkdirs();
+          new File(String.valueOf(target)).mkdirs();
           if (!destination.toFile().isDirectory()) {
             Files.copy(child, destination, StandardCopyOption.REPLACE_EXISTING);
           }
-        } catch (Exception ex) {
-          ex.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
         }
       });
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      System.out.println(
+          "Неправильно указан адрес папки источника или папки назначения. Введите еще раз.");
+      check = false;
+    }
+    if (check == true) {
+      System.out.println("Копирование выполнено успешно");
     }
   }
 }
+
 
