27fe511df813c47750918db863ae902180da23e4
Yustas <goosseff@yandex.ru>
2020-08-06 18:45:39 +0300

    recieve & fetch 2.0

--- 09_FilesAndNetwork/HtmlParsing/src/main/java/Main.java
+++ 09_FilesAndNetwork/HtmlParsing/src/main/java/Main.java
@@ -1,8 +1,12 @@
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,41 +14,33 @@ import org.jsoup.nodes.Element;
 
 public class Main {
 
-  private static final String IMAGE_DESTINATION_FOLDER = "C:/Images from URL";
+  private static final String IMAGE_DESTINATION_FOLDER = "c:/images";
+  private static final String URL_NEED = "https://lenta.ru/";
 
   public static void main(String[] args) {
 
-    String urlNeed = "https://lenta.ru/";
+    new File(String.valueOf(IMAGE_DESTINATION_FOLDER)).mkdirs();
     Document docLenta = null;
     try {
-      docLenta = Jsoup.connect(urlNeed).get();
+           docLenta = Jsoup
+          .connect(URL_NEED)
+          .userAgent("Mozilla/5.0")
+          .timeout(10 * 1000)
+          .get();
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-    //StringBuilder stringBuilder = new StringBuilder("");
     ArrayList<String> listImg = new ArrayList<>();
 
-
-    int i = 0;
-    for(Element docs : docLenta.select("img")){
+    for(Element docs : docLenta.select("img"))
+    {
       listImg.add(docs.attr("abs:src")+"\n");
-
-//      ++i;
-//      System.out.println(docs);
-//      System.out.println(i);
     }
-
-    //listImg.stream().forEach(System.out::println);
 
     for (String e : listImg) {
       downloadImage(e);
     }
-
-//    String title = docLenta.title();
-//
-//    System.out.println(title);
-
   }
 
   private static void downloadImage(String strImageURL) {
@@ -55,24 +51,30 @@ public class Main {
     System.out.println("Saving: " + strImageName + ", from: " + strImageURL);
 
     try {
-
-      //open the stream from URL
       URL urlImage = new URL(strImageURL);
-      InputStream in = urlImage.openStream();
+      InputStream input = urlImage.openStream();
 
-      byte[] buffer = new byte[4096];
-      int n = -1;
+      String s = IMAGE_DESTINATION_FOLDER + "/" + strImageName;
+      System.out.println(s);
 
-      OutputStream os =
-          new FileOutputStream( IMAGE_DESTINATION_FOLDER + "/" + strImageName );
+      Path path = Path.of(s);
+      Files.copy(input, path, StandardCopyOption.REPLACE_EXISTING);
 
-      //write bytes to the output stream
-      while ( (n = in.read(buffer)) != -1 ){
-        os.write(buffer, 0, n);
-      }
 
-      //close the stream
-      os.close();
+//
+//      byte[] buffer = new byte[4096];
+//      int n = -1;
+//
+//      OutputStream os =
+//          new FileOutputStream( IMAGE_DESTINATION_FOLDER + "/" + strImageName );
+//
+//      //write bytes to the output stream
+//      while ( (n = in.read(buffer)) != -1 ){
+//        os.write(buffer, 0, n);
+//      }
+//
+//      //close the stream
+//      os.close();
 
       System.out.println("Image saved");
 
