7997a61b47e568cc4936ec61e27308c0e2843e2b
Yustas <goosseff@yandex.ru>
2020-10-10 13:50:36 +0300

    working code 1.0

--- 10_SQLAndHibernate/JDBCExperiments/src/main/java/jdbsPackage/Main.java
+++ 10_SQLAndHibernate/JDBCExperiments/src/main/java/jdbsPackage/Main.java
@@ -9,31 +9,35 @@ import java.sql.Statement;
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    String url = "jdbc:mysql://localhost:3306/skillbox1?useSSL=false&serverTimezone=Europe/Moscow&useUnicode=true&characterEncoding=utf8";
+    String user = "root";
+    String password = "testtest";
 
-      String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=Europe/Moscow&useUnicode=true&amp;characterEncoding=utf8";
-      String user = "root";
-      String password = "testtest";
+    try {
+      Connection connection = DriverManager.getConnection(url, user, password);
 
-      try {
-        Connection connection = DriverManager.getConnection(url, user, password);
+      Statement statement = connection.createStatement();
 
-        Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery(
+          "SELECT course_name, cnt/sales_period as AVGMONTHSALES FROM (SELECT course_name, YEAR(subscription_date),"
+              + " (MONTH(max(subscription_date)) - month(min(subscription_date))+1) as sales_period, COUNT(*) AS cnt "
+              + "FROM purchaselist GROUP BY 1) foo GROUP BY 1;");
 
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM Courses");
-        while (resultSet.next()) {
+      System.out.println("Среднее количество покупок в месяц для каждого курса за 2018 год" + "\n");
 
-          String courseName = resultSet.getString("name");
-          System.out.println(courseName);
-        }
-        resultSet.close();
-        statement.close();
-        connection.close();
+      while (resultSet.next()) {
 
-      } catch (SQLException ex) {
-        ex.printStackTrace();
+        String courseName = resultSet.getString("course_name");
+        String avg_month_sales = resultSet.getString("AVGMONTHSALES");
+        System.out.println(courseName + "    " + avg_month_sales);
       }
+      resultSet.close();
+      statement.close();
+      connection.close();
 
-
+    } catch (SQLException ex) {
+      ex.printStackTrace();
     }
   }
+}
