d6c0424a1f273b43517bf465e7250c7386d52683
Yustas <goosseff@yandex.ru>
2020-12-26 21:51:54 +0300

    Interim code 1.5

--- 11_Multithreading/Transactions/src/Account.java
+++ 11_Multithreading/Transactions/src/Account.java
@@ -2,4 +2,38 @@ public class Account
 {
     private long money;
     private String accNumber;
+
+    public Account() {
+    }
+
+    public Account(long money, String accNumber) {
+        this.money = money;
+        this.accNumber = accNumber;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public void setMoney(long money) {
+        this.money = money;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Для счета № " + accNumber + "  " +
+            "Остаток на счете составляет - '" + money +
+            "'";
+    }
+
+
+
 }
--- 11_Multithreading/Transactions/src/Bank.java
+++ 11_Multithreading/Transactions/src/Bank.java
@@ -1,17 +1,27 @@
 import java.util.HashMap;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Bank
 {
-    private HashMap<String, Account> accounts;
+    private ConcurrentHashMap<String, Account> accounts;
     private final Random random = new Random();
 
-    public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
+    public synchronized boolean isFraud(long amount)
         throws InterruptedException
     {
         Thread.sleep(1000);
         return random.nextBoolean();
     }
+
+    public ConcurrentHashMap<String, Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(ConcurrentHashMap<String, Account> accounts) {
+        this.accounts = accounts;
+    }
+
 
     /**
      * TODO: реализовать метод. Метод переводит деньги между счетами.
@@ -20,16 +30,54 @@ public class Bank
      * метод isFraud. Если возвращается true, то делается блокировка
      * счетов (как – на ваше усмотрение)
      */
-    public void transfer(String fromAccountNum, String toAccountNum, long amount)
-    {
+    public void transfer(Bank bank, String fromAccountNum, String toAccountNum, long amount) {
+        boolean fraud = false;
+        if (amount > 50000) {
+            try {
+                fraud = isFraud(amount);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.err.println("Occurred aborting the transaction");
+                return;
+            }
+        }
 
+        if (!fraud) {
+            long fromAccountBalance = bank.getBalance(bank, fromAccountNum);
+            long toAccountBalance = bank.getBalance(bank, toAccountNum);
+
+            long fromAccountBalanceNew = fromAccountBalance - amount;
+            long toAccountBalanceNew = toAccountBalance + amount;
+
+            // используем метод replace (ключ K, значение V)
+            ConcurrentHashMap<String, Account> currentBank = bank.getAccounts();
+
+            Account fromAccount = currentBank.get(fromAccountNum);
+            fromAccount.setMoney(fromAccountBalanceNew);
+            currentBank.replace(fromAccountNum, fromAccount);
+
+            Account toAccount = currentBank.get(toAccountNum);
+            toAccount.setMoney(toAccountBalanceNew);
+            currentBank.replace(toAccountNum, toAccount);
+        }
+        else {
+            // 
+        }
     }
 
     /**
-     * TODO: реализовать метод. Возвращает остаток на счёте.
+     * Метод возвращает остаток на счёте.
      */
-    public long getBalance(String accountNum)
-    {
-        return 0;
+    public static long getBalance(Bank bank, String accountNum) {
+        ConcurrentHashMap<String, Account> currentBank = bank.getAccounts();
+        long accountBalance = 0;
+        if (currentBank.containsKey(accountNum)) {
+
+            accountBalance = currentBank.get(accountNum).getMoney();
+
+        } else {
+            System.out.println("Указанного номера счета не существует");
+        }
+        return accountBalance;
     }
 }
