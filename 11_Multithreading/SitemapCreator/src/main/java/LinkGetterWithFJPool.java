3e804bc0ccff363e0948f98ca23a1cd15cb5b793
Yustas <goosseff@yandex.ru>
2021-02-27 23:30:19 +0300

    Interim code 2.3

--- 11_Multithreading/SitemapCreator/src/main/java/LinkGetterWithFJPool.java
+++ 11_Multithreading/SitemapCreator/src/main/java/LinkGetterWithFJPool.java
@@ -1,10 +1,43 @@
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
-public class LinkGetterWithFJPool extends RecursiveTask<List<String>> {
+public class LinkGetterWithFJPool extends RecursiveTask<List<Element>> {
+
+  LinkedList<Element> listURL = new LinkedList<>();
+
+  public LinkGetterWithFJPool(LinkedList<Element> listURL) {
+    this.listURL = listURL;
+  }
 
   @Override
-  protected List<String> compute() {
+  protected List<Element> compute() {
+
+    Document docNeed = null;
+    try {
+      docNeed = Jsoup
+          .connect(Main.URL_SKILLBOX)
+          .userAgent("Mozilla/5.0")
+          .timeout(10 * 1000)
+          .get(); }
+    catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Ошибка при парсинге страницы");
+    }
+
+    for (Element docs : docNeed  // в коде страницы
+        .select("a[href]"))  // осуществляем поиск элементов соответствующих требованию
+    {
+      listURL.add(docs); // прибавляем найденные элементы в список
+      return listURL;
+    }
+
+
+
 
     return null;
   }
--- 11_Multithreading/SitemapCreator/src/main/java/Main.java
+++ 11_Multithreading/SitemapCreator/src/main/java/Main.java
@@ -1,45 +1,65 @@
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.regex.Pattern;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 
 
 public class Main {
 
-  private static final String URL_NEED = "https://lenta.ru/";
+  public static final String URL_NEED = "https://lenta.ru/";
+  public static final String URL_SKILLBOX = "https://Skillbox.ru/";
 
   public static void main(String[] args) {
 
-    LinkGetter linkGetter = new LinkGetter();
-    List needUrlList = linkGetter.getLinks(URL_NEED);
-
-    for (Object s : needUrlList) {
-          System.out.println(s);
-        }
-
-
-
-
-
-
-//    Document docLenta = null;
-//    try {
-//      docLenta = Jsoup
-//          .connect(URL_NEED)
-//          .userAgent("Mozilla/5.0")
-//          .timeout(10 * 1000)
-//          .get();
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//      System.out.println("Ошибка при парсинге страницы");
-//      return;
-//    }
+//    LinkGetter linkGetter = new LinkGetter();
+//    List needUrlList = linkGetter.getLinks(URL_NEED);
 //
+//    for (Object s : needUrlList) {
+//          System.out.println(s);
+//        }
+
+  // ===============================================================
+
+    LinkedList<String> listURL = new LinkedList<>();
+    List<Node> listUrlElement = new LinkedList<>();
+
+    Pattern linkSkillbox = Pattern.compile("\"https://skillbox.ru/*\"");
+
+    Document docNeed = null;
+    try {
+      docNeed = Jsoup
+          .connect(Main.URL_SKILLBOX)
+          .userAgent("Mozilla/5.0")
+          .timeout(10 * 1000)
+          .get(); }
+    catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Ошибка при парсинге страницы");
+    }
+
+    for (Element docs : docNeed  // в коде страницы
+        .select("a[href]"))  // осуществляем поиск элементов соответствующих требованию
+    {
+      listUrlElement = docs.childNodes();
+      //listURL.add(docs.attr("abs:href")); // прибавляем найденные элементы в список
+      //listURL.forEach(System.out::println);
+
+//      listUrlElement = docs.childNodesCopy();
+      listUrlElement.forEach(System.out::println);
+    }
+
+
+    for (Node node : listUrlElement)
+    {
+      listURL.add(node.attr("abs:href"));
+      listURL.forEach(System.out::println);
+    }
+
 //    LinkedList<Element> listURL = new LinkedList<>();
 //    LinkedList<String> listURLtext = new LinkedList<>();
 
