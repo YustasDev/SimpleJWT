a2c35886ee432ba4b020cc82cb4342aab384f40e
Yustas <goosseff@yandex.ru>
2021-04-06 17:19:47 +0300

    Interim code 3.1

--- 11_Multithreading/SitemapCreator/src/main/java/Main.java
+++ 11_Multithreading/SitemapCreator/src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -16,26 +19,27 @@ import org.jsoup.select.Elements;
 
 
 public class Main {
-
-  //public static final String URL_NEED = "https://lenta.ru/";
+  
   public static final String URL_NEED = "http://sendel.ru/";
   public static final String URL_SKILLBOX = "https://Skillbox.ru/";
 
   public static void main(String[] args) {
 
-//    LinkGetter linkGetter = new LinkGetter();
-//    List needUrlList = linkGetter.getLinks(URL_NEED);
-//
-//    for (Object s : needUrlList) {
-//          System.out.println(s);
-//        }
-    // ===============================================================
 
     List<String> resultList = new ForkJoinPool()
         .invoke(new LinkGetterWithFJPool(URL_NEED));
-    System.out.println(resultList);
+    System.out.println("Все найденные URL: " + resultList);
+    Set<String> nonDuplicates = findDuplicates(resultList);
+    System.out.println("Без дубликатов:");
+    nonDuplicates.forEach((e) -> { System.out.println(e); });
 
+  }
 
+  public static Set<String> findDuplicates(Collection<String> collection) {
+    Set<String> elements = new HashSet<>();
+    return collection.stream()
+        .filter(e -> elements.add(e))
+        .collect(Collectors.toSet());
   }
 }
 
