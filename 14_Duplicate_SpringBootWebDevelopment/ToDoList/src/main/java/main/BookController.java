eac3ed8300845431f2f91223cbf9cf0a70e2fc75
Yustas <goosseff@yandex.ru>
2021-05-08 21:32:36 +0300

    MyToDoList interim code 1.1

--- 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/BookController.java
+++ 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/BookController.java
@@ -1,20 +1,22 @@
 package main;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import response.Book;
 
 @RestController
 public class BookController {
 
+  @RequestMapping(value = "/books/", method = RequestMethod.GET)
   public List<Book> list(){
-
-    return null;
+    return Storage.getAllBooks();
   }
 
+  @RequestMapping(value = "/books/", method = RequestMethod.POST)
   public int add(Book book) {
-
-
+    return Storage.addBook(book);
   }
 
 }
