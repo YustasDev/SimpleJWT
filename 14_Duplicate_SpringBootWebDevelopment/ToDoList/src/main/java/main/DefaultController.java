4b9716e3e44214931bb8d91eaa0af2fd9755480a
Yustas <goosseff@yandex.ru>
2021-05-20 00:06:49 +0300

    ToDoList with View interim code 3.0

--- 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,16 +1,30 @@
 package main;
 
 import java.util.Date;
+import java.util.List;
+
+import main.model.CaseRepository;
+import main.model.CriminalCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Streamable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DefaultController {
 
-  @RequestMapping("/")
-  public String index() {
+  @Autowired
+  CaseRepository caseRepository;
 
-    return ("Hooray, everything works in   " + (new Date()).toString());
+  @RequestMapping("/")
+  public String index(Model model) {
+    Iterable<CriminalCase> criminalCaseIterable = caseRepository.findAll();
+    List<CriminalCase> listOfCase = Streamable.of(criminalCaseIterable).toList();
+    model.addAttribute("listOfCase", listOfCase);
+    model.addAttribute("СaseCount", listOfCase.size());
+    return "index";
   }
 }
 
