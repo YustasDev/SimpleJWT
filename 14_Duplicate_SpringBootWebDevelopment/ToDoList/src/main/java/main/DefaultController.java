aba7e31931822f684d43c52030690f6d1b736583
Vitaliy Goosseff <vitaly.gusev@progforce.com>
2021-05-23 14:15:56 +0300

    ToDoList with View_component interim code 3.5

--- 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import main.model.CaseRepository;
 import main.model.CriminalCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,14 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
 
   @Autowired
-  CaseRepository caseRepository;
+  private CaseRepository caseRepository;
+
+  @Value("${someParameter}")
+  private Integer someParameter;
 
   @RequestMapping("/")
   public String index(Model model) {
     Iterable<CriminalCase> criminalCaseIterable = caseRepository.findAll();
     List<CriminalCase> listOfCase = Streamable.of(criminalCaseIterable).toList();
     model.addAttribute("listOfCase", listOfCase);
-    model.addAttribute("СaseCount", listOfCase.size());
+    model.addAttribute("caseCount", listOfCase.size());
+    model.addAttribute("someParameter", someParameter);
     return "index";
   }
 }
