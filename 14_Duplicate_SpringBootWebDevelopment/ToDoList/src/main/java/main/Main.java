b2eb95ccb4e2931bcab506e5a6bf88d170606bbf
Vitaliy Goosseff <vitaly.gusev@progforce.com>
2021-05-30 22:41:24 +0300

    ToDoList with View_component code 3.6

--- 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/Main.java
+++ 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,11 +9,5 @@ public class Main {
   public static void main(String[] args) {
 
     SpringApplication.run(Main.class, args);
-
-
-
-
   }
-
-
 }
