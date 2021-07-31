304f9b590f23d3e8405d0b2cc03d3a204080e694
Vitaliy Goosseff <vitaly.gusev@progforce.com>
2021-08-01 01:40:54 +0300

    I don't know how to work with a MongoCollection<Document>

--- 15_NoSQL/MongoDBtest/src/main/java/MongoDBtest.java
+++ 15_NoSQL/MongoDBtest/src/main/java/MongoDBtest.java
@@ -1,58 +1,82 @@
+import static com.mongodb.client.model.Aggregates.limit;
+import static com.mongodb.client.model.Aggregates.sample;
+import static com.mongodb.client.model.Aggregates.sort;
+import static com.mongodb.client.model.Aggregates.sortByCount;
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 public class MongoDBtest {
 
   public static void main(String[] args) {
     MongoClient mongoClient = new MongoClient( "127.0.0.1" , 27017 );
 
-    MongoDatabase database = mongoClient.getDatabase("local");
+    MongoDatabase database = mongoClient.getDatabase("my_books");
 
-    // Создаем коллекцию
-    MongoCollection<Document> collection = database.getCollection("TestMongoDBDemo");
+    MongoCollection<Document> collection = database.getCollection("books");
 
-    // Удалим из нее все документы
     collection.drop();
 
-    // Создадим первый документ
-    Document firstDocument = new Document()
-        .append("Type", 1)
-        .append("Description", "Это мой первый документ в MongoDB")
-        .append("Author", "Me")
-        .append("Time", new SimpleDateFormat().format(new Date()));
+    Document book1 = new Document()
+        .append("Book_title", "Игра в бисер")
+        .append("Author", "Герман Гессе")
+        .append("Year_of_publication", 1971);
+
+    Document book2 = new Document()
+        .append("Book_title", "Степной волк")
+        .append("Author", "Герман Гессе")
+        .append("Year_of_publication", 1965);
+
+    Document book3 = new Document()
+        .append("Book_title", "Морской волк")
+        .append("Author", "Джек Лондон")
+        .append("Year_of_publication", 1972);
+
+    Document book4 = new Document()
+        .append("Book_title", "Человек в поисках смысла")
+        .append("Author", "Виктор Франкл")
+        .append("Year_of_publication", 1985);
+
+    Document book5 = new Document()
+        .append("Book_title", "Мастер и Маргарита")
+        .append("Author", "Михаил Булгаков")
+        .append("Year_of_publication", 1971);
+
+    List<Document> listOfBooks = new ArrayList<>();
+    listOfBooks.add(book1);
+    listOfBooks.add(book2);
+    listOfBooks.add(book3);
+    listOfBooks.add(book4);
+    listOfBooks.add(book5);
+
+    collection.insertMany(listOfBooks);
+
+    //var bookNeed = database.getCollection("books").find().sort({"Year_of_publication" : -1}).limit(1);
+
+    //database.getCollection("books").aggregate( [ { $project : { Year_of_publication : 1 } } ] )
+
+    var year = collection.find(new BsonDocument()).filter(sortByCount("Year_of_publication"));
+
+    System.out.println(year.toString());
+
+    collection.aggregate(listOfBooks).     // ????
 
 
-    // Вложенный объект
-    Document nestedObject = new Document()
-        .append("Course", "NoSQL Базы Данных")
-        .append("Author", "Yustas Goosseff");
+        //min(y -> y.getField("Year_of_publication"));
 
-    firstDocument.append("Skillbox", nestedObject);
+        //sort(y -> y.getField("Year_of_publication");
+
+   // collection.aggregate([ { $project : { Year_of_publication : 1 } } ])
 
 
-    // Вставляем документ в коллекцию
-    collection.insertOne(firstDocument);
 
-    collection.find().forEach((Consumer<Document>) document -> {
-      System.out.println("Это первый документ:\n" + document);
-    });
 
-    // Используем JSON-синтаксис для создания объекта
-    Document secondDocument = Document.parse(
-        "{Type: 2, Description:\"Этот документ создан с помощью JSON-синтаксиса\"}"
-    );
-    collection.insertOne(secondDocument);
-
-    // Используем JSON-синтаксис для написания запроса (выбираем документы с Type=2)
-    BsonDocument query = BsonDocument.parse("{Type: {$eq: 2}}");
-    collection.find(query).forEach((Consumer<Document>) document -> {
-      System.out.println("Это второй документ:\n" + document);
-    });
   }
 }
