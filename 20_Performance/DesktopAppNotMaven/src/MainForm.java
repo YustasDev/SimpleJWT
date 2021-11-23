846781e191489d3445e20a6fe7ac1dc8d0c98c9e
Викторович Виталий <goosseff@yandex.ru>
2021-11-23 21:17:15 +0300

    DesktopAppNotMaven 0.1

---
+++ 20_Performance/DesktopAppNotMaven/src/MainForm.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class MainForm {
+
+  private JPanel mainPanel;
+  private JTextArea textArea1;
+  private JButton countButton;
+  private JButton clearItButton;
+
+  public JPanel getMainPanel(){
+    return mainPanel;
+  }
+}
