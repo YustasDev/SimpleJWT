04440e33ac251a40ce7579c054502c434880cea5
Викторович Виталий <goosseff@yandex.ru>
2021-11-23 17:28:33 +0300

    DesktopApp 0.1

---
+++ 20_Performance/DesktopApp/src/main/java/MainForm.java
@@ -0,0 +1,20 @@
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class MainForm {
+
+  private JPanel mainPanel;
+  private JTextArea textArea1;
+  private JButton countItButton;
+  private JPanel countBatton;
+  private JButton clearButton;
+
+  public JPanel getMainPanel(){
+    return mainPanel;
+  }
+
+  private void createUIComponents() {
+    // TODO: place custom component creation code here
+  }
+}
