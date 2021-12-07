ef388995ccafe499aef2c6f0186eb5eaa8c5f2f5
Vitaliy Goosseff <vitaly.gusev@progforce.com>
2021-12-07 11:09:01 +0300

    Interim code 0.1

--- 20_Performance/DesktopAppNotMaven/src/Main.java
+++ 20_Performance/DesktopAppNotMaven/src/Main.java
@@ -10,13 +10,11 @@ public class Main {
     JFrame frame = new JFrame();
     frame.setSize(600, 400);
 
+    frame.add(new MainForm().getNamePanel());
     frame.add(new MainForm().getMainPanel());
-
 
     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
-
-
   }
 }
