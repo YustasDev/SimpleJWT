bf4a0068e115b5e3bdd9b16cdb0cdd16f12d9f80
progforce <vitaly.gusev@progforce.com>
2022-04-05 14:26:45 +0300

    Working code for Stage 3. Releas 3.2

--- 26_SearchEngine/src/main/java/services/Morphology.java
+++ 26_SearchEngine/src/main/java/services/Morphology.java
@@ -1,41 +1,47 @@
 package services;
 
 import org.apache.lucene.morphology.LuceneMorphology;
-import org.apache.lucene.morphology.english.EnglishAnalyzer;
-import org.apache.lucene.morphology.russian.RussianAnalyzer;
 import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Morphology {
 
+    private static Map<List<String>, Integer> lemmMap = new LinkedHashMap<>();
+
     public static void main(String[] args) throws IOException {
 
-        LuceneMorphology luceneMorph = new RussianLuceneMorphology();
-//        List<String> wordBaseForms = luceneMorph.getNormalForms("леса");
-//        wordBaseForms.forEach(System.out::println);
 
-        List<String> wordBaseForms = luceneMorph.getMorphInfo("или");
-        wordBaseForms.forEach(System.out::println);
-
-
-
+        //getSetLemmas("Вася или Петя пошел в лес, а также в поле; лишь Саша не пошел - но он почти герой");
+        Map<List<String>, Integer> lemTextMap = getSetLemmas("Повторное появление леопарда в Осетии позволяет предположить, что леопард постоянно обитает в некоторых районах Северного Кавказа.");
+        System.out.println(lemTextMap);
     }
 
-    private static Set<String> getSetLemmas(String text) throws IOException {
-        RussianAnalyzer analyzer = new RussianAnalyzer();
+    private static Map<List<String>, Integer> getSetLemmas(String text) throws IOException {
+        //RussianAnalyzer analyzer = new RussianAnalyzer();
+        LuceneMorphology luceneMorph = new RussianLuceneMorphology();
+        String[] disassembledText = text.trim().split("\\s+");
 
-
-
-
-
-
-
-
-
-        return null;
+        for (String str : disassembledText) {
+            String s = str.toLowerCase().replaceAll("[\\p{Punct}\\s&&[^\\h]]", "");
+            if (!(s == null || s.isEmpty() || s.trim().isEmpty())){
+                List<String> wordBaseForms = luceneMorph.getMorphInfo(s);
+                for (String word : wordBaseForms){
+                if (!(word.contains("СОЮЗ") || word.contains("МЕЖД") || word.contains("ПРЕДЛ") || word.contains("ЧАСТ"))) {
+                    List<String> lemmaForms = luceneMorph.getNormalForms(s);
+                    Integer countlemm = lemmMap.get(lemmaForms);
+                    if (countlemm == null){
+                        lemmMap.put(lemmaForms, 1);
+                        continue;
+                    }
+                    else {countlemm++;
+                    lemmMap.replace(lemmaForms, countlemm);}
+                }
+              }
+            }
+        }
+        return lemmMap;
     }
 
 }
