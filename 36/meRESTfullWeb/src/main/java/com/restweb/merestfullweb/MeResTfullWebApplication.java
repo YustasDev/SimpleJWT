c7b252bdb29ff90c4a48d6e9bb8bb9c5dfbdbec1
progforce <vitaly.gusev@progforce.com>
2022-10-21 14:55:58 +0300

    init myRESTFull API release 0.1

---
+++ 36/meRESTfullWeb/src/main/java/com/restweb/merestfullweb/MeResTfullWebApplication.java
@@ -0,0 +1,13 @@
+package com.restweb.merestfullweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeResTfullWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeResTfullWebApplication.class, args);
+    }
+
+}
