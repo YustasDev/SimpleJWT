12f4508d86ab64affe834ff8a41a609ac86f1237
progforce <vitaly.gusev@progforce.com>
2022-12-20 17:11:18 +0300

    + new banner.txt

--- 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/Main.java
+++ 14_Duplicate_SpringBootWebDevelopment/ToDoList/src/main/java/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,7 +15,16 @@ public class Main {
 
 
   public static void main(String[] args) {
-    SpringApplication.run(Main.class, args);
+   // SpringApplication.run(Main.class, args);
+
+    SpringApplication app = new SpringApplication (Main.class);
+    app.setBanner(new Banner() {
+    @Override
+    public void printBanner(Environment environment, Class<?> sourceclass, PrintStream out) {
+        out.print("\n\n\tThis is my own banner!\n\n".toUpperCase());
+      }
+    });
+    app.run(args);
   }
 
   @Scheduled(cron="0 0 10 * * *")
