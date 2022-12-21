65fdcc701907c7063519635b1fa12b33f1167636
progforce <vitaly.gusev@progforce.com>
2022-12-21 14:31:37 +0300

    init 0.1

---
+++ 31_myWebFlux/src/main/java/com/example/its_31_mywebflux/Its31MyWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.its_31_mywebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Its31MyWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Its31MyWebFluxApplication.class, args);
+    }
+
+}
