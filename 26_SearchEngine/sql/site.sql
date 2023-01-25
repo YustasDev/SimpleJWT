80461645c7f6a4fca0b2705c73265cad2334705c
progforce <vitaly.gusev@progforce.com>
2023-01-25 16:10:02 +0300

    init 0.1

---
+++ 26_SearchEngine/sql/site.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `site` (
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `status` enum('INDEXING','INDEXED','FAILED') NOT NULL,
+                        `status_time` datetime NOT NULL,
+                        `last_error` text,
+                        `url` varchar(255) NOT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ 26_WebService/src/main/java/com/example/webservice_for_searchengine/WebServiceForSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservice_for_searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceForSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceForSearchEngineApplication.class, args);
+    }
+
+}
